d66168bff4784465591db6cf1300c5b850e9ba76
Yefta Sutanto <y458m@yahoo.com>
2014-01-22 00:12:28 +0700

    Added More Commands

--- commands/slap/__init__.py
+++ commands/slap/__init__.py
@@ -3,5 +3,7 @@ import logging
 def slap_run_cmd(line, config):
     logger = logging.getLogger('bot.cmd.slap')
     userNick = line.split(':')[2].split(' ')
-    logger.info('Completed command')
-    return 'PRIVMSG ' + config['channel'] + ' : Gan ' + userNick + ', dikaplok tuh~'
+    if len(userNick) == 2:
+        userNick = userNick[1]
+        logger.info('Completed command')
+        return 'PRIVMSG ' + config['channel'] + ' : Gan ' + userNick + ', dikaplok tuh~'
